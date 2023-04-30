9487c6eb977c4c174ef574443718fd4639cafb7d
Victorino04 <miguel4victorino@gmail.com>
2023-04-30 22:41:48 +0100

    corrected share for sealed persons

--- src/Sealed.java
+++ src/Sealed.java
@@ -10,17 +10,16 @@ public class Sealed extends PersonClass{
     public void shareGossips(Person other) {
         boolean shared = false;
         while (!shared) {
-            for (int i = 0; i < gossips.size(); i++) {
-                Gossip share = gossips.get(i);
-                if (!other.knowsGossip(share) && share.isAbout(this)) {
-                    other.addGossip(share);
-                }
-                gossips.remove(share);
-                gossips.insertLast(share);
-                oldest--;
-                if (oldest < 0) { oldest = gossips.size() - 1; }
+            Gossip share = gossips.get(0);
+            if (!other.knowsGossip(share) && share.isAbout(this)) {
+                other.addGossip(share);
+                shared = true;
             }
-        }
+            gossips.remove(share);
+            gossips.insertLast(share);
+            oldest--;
+            if (oldest < 0) { oldest = gossips.size() - 1; }
+            }
     }
 
 
