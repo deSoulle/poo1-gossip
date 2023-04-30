1b5fca0fb1b39ff0aabf64459ed9b4675aeb3b38
Victorino04 <miguel4victorino@gmail.com>
2023-04-30 16:25:56 +0100

    corrections

--- src/Gossiper.java
+++ src/Gossiper.java
@@ -30,14 +30,11 @@ public class Gossiper extends PersonClass{
     public Iterator<Gossip> sharedIterator() {
         Array<Gossip> shared = new ArrayExt<>();
         int num = Math.min(gossips.size(), 3);
-        int tmp = last;
+        int tmp = last - 1;
 
-        for(int i = num; i > 0; i --) {
-            shared.insertLast(gossips.get(tmp --));
-            if (tmp == 0) {
-                tmp = gossips.size() - 1;
+        for(int i = 0; i < num; i ++) {
+            shared.insertAt(gossips.get(tmp --), i);
             }
-        }
 
         return shared.iterator();
     }
