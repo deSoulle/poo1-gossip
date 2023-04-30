9267cf96b5dec4115ce7f72ac7a7e75ff91a455c
Victorino04 <miguel4victorino@gmail.com>
2023-04-30 18:24:10 +0100

    corrections

--- src/Gossiper.java
+++ src/Gossiper.java
@@ -31,9 +31,10 @@ public class Gossiper extends PersonClass{
         int num = Math.min(gossips.size(), 3);
         int tmp = last - 1;
 
-        for(int i = 0; i < num; i ++) {
-            if (tmp < 0) { tmp = gossips.size() - 1; }
-            shared.insertAt(gossips.get(tmp --), i);
+        for(int i = num; i > 0; i --) {
+            tmp -= i;
+            if (tmp < 0) { tmp = gossips.size() + tmp - i; }
+            shared.insertLast(gossips.get(tmp));
         }
 
         return shared.iterator();
