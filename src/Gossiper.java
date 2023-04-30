6388a5ec552687f2814b5130ada6581020445990
deSoulle <af.nunosousa@gmail.com>
2023-04-30 19:17:47 +0100

    enters (visual)

--- src/Gossiper.java
+++ src/Gossiper.java
@@ -31,10 +31,18 @@ public class Gossiper extends PersonClass{
         int num = Math.min(gossips.size(), 3);
         int tmp = last - 1;
 
+<<<<<<< Updated upstream
         for(int i = num; i > 0; i --) {
             tmp -= i;
             if (tmp < 0) { tmp = gossips.size() + tmp - i; }
             shared.insertLast(gossips.get(tmp));
+=======
+        for(int i = 0; i < num; i ++) {
+            if (tmp < 0) {
+                tmp = gossips.size() - 1;
+            }
+            shared.insertAt(gossips.get(tmp --), i);
+>>>>>>> Stashed changes
         }
 
         return shared.iterator();
