7c9df602d95f5408ca2c6c262dbf8fbbd6492799
Victorino04 <miguel4victorino@gmail.com>
2023-04-30 22:24:51 +0100

    share for sealed persons

--- src/Main.java
+++ src/Main.java
@@ -459,7 +459,7 @@ public class Main {
         else if(!community.knowsGossips(name)) {
             System.out.println(name + " knows nothing!");
         }
-        else if(community.hasAboutThem(name)) {
+        else if(!community.hasAboutThem(name)) {
             System.out.println(name + " does not wish to gossip right now!");
         }
         else {
--- src/Sealed.java
+++ src/Sealed.java
@@ -6,6 +6,23 @@ public class Sealed extends PersonClass{
         super(name);
     }
 
+    @Override
+    public void shareGossips(Person other) {
+        boolean shared = false;
+        while (!shared) {
+            for (int i = 0; i < gossips.size(); i++) {
+                Gossip share = gossips.get(i);
+                if (!other.knowsGossip(share) && share.isAbout(this)) {
+                    other.addGossip(share);
+                }
+                gossips.remove(share);
+                gossips.insertLast(share);
+                oldest--;
+                if (oldest < 0) { oldest = gossips.size() - 1; }
+            }
+        }
+    }
+
 
 }
 
